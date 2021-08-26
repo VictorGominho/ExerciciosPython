8d1b1683491c5d20c895e29f76eedcec58e80a39
VictorGominho <victor_goma@hotmail.com>
2021-08-26 19:31:41 -0300

    Create Aula 1.03.py
    Adiciona o primeiro exercício dos Collab notebooks do NexT

---
+++ NexT Collab Notebooks/Aula 1.03.py
@@ -0,0 +1,5 @@
+#Faça um Programa que peça dois números e imprima a soma.
+
+x = int(input('Digite o primeiro número: '))
+y = int(input('Digite o segundo número: '))
+print('A soma dos dois números é', x+y)
